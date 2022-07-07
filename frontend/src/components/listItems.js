a0d2f5af0c76d3cf36878ab509cc6bb74e89e69a
rafael-14 <fcramos12@hotmail.com>
2022-07-07 13:49:28 -0300

    Link tag on listItems

--- frontend/src/components/listItems.js
+++ frontend/src/components/listItems.js
@@ -6,44 +6,57 @@ import PersonIcon from '@mui/icons-material/Person';
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
 import PlumbingIcon from '@mui/icons-material/Plumbing';
+import Link from 'next/link'
 
 export const mainListItems = (
   <>
-    <ListItem button component="a" href="/listar/pedidos" >
-      <ListItemIcon >
-        <ReceiptIcon style={{ color: "#000000" }} />
-      </ListItemIcon>
-      <ListItemText primary="Pedidos" primaryTypographyProps={{ color: "#000000" }} />
-    </ListItem>
-    <ListItem button component="a" href="/listar/processos" >
-      <ListItemIcon >
-        <PrecisionManufacturingIcon style={{ color: "#000000" }} />
-      </ListItemIcon>
-      <ListItemText primary="Processos" primaryTypographyProps={{ color: "#000000" }} />
-    </ListItem>
-    <ListItem button component="a" href="/" >
-      <ListItemIcon >
-        <PlumbingIcon style={{ color: "#000000" }} />
-      </ListItemIcon>
-      <ListItemText primary="Produção" primaryTypographyProps={{ color: "#000000" }} />
-    </ListItem>
-    <ListItem button component="a" href="/listar/produtos" >
-      <ListItemIcon >
-        <DiamondIcon style={{ color: "#000000" }} />
-      </ListItemIcon>
-      <ListItemText primary="Produtos" primaryTypographyProps={{ color: "#000000" }} />
-    </ListItem>
-    <ListItem button component="a" href="/listar/setores" >
-      <ListItemIcon >
-        <ViewModuleIcon style={{ color: "#000000" }} />
-      </ListItemIcon>
-      <ListItemText primary="Setores" primaryTypographyProps={{ color: "#000000" }} />
-    </ListItem>
-    <ListItem button component="a" href="/listar/usuarios" >
-      <ListItemIcon >
-        <PersonIcon style={{ color: "#000000" }} />
-      </ListItemIcon>
-      <ListItemText primary="Usuários" primaryTypographyProps={{ color: "#000000" }} />
-    </ListItem>
+    <Link href="/listar/pedidos">
+      <ListItem button component="a">
+        <ListItemIcon >
+          <ReceiptIcon style={{ color: "#000000" }} />
+        </ListItemIcon>
+        <ListItemText primary="Pedidos" primaryTypographyProps={{ color: "#000000" }} />
+      </ListItem>
+    </Link>
+    <Link href="/listar/processos">
+      <ListItem button component="a">
+        <ListItemIcon >
+          <PrecisionManufacturingIcon style={{ color: "#000000" }} />
+        </ListItemIcon>
+        <ListItemText primary="Processos" primaryTypographyProps={{ color: "#000000" }} />
+      </ListItem>
+    </Link>
+    <Link href="/">
+      <ListItem button component="a">
+        <ListItemIcon >
+          <PlumbingIcon style={{ color: "#000000" }} />
+        </ListItemIcon>
+        <ListItemText primary="Produção" primaryTypographyProps={{ color: "#000000" }} />
+      </ListItem>
+    </Link>
+    <Link href="/listar/produtos">
+      <ListItem button component="a">
+        <ListItemIcon >
+          <DiamondIcon style={{ color: "#000000" }} />
+        </ListItemIcon>
+        <ListItemText primary="Produtos" primaryTypographyProps={{ color: "#000000" }} />
+      </ListItem>
+    </Link>
+    <Link href="/listar/setores">
+      <ListItem button component="a">
+        <ListItemIcon >
+          <ViewModuleIcon style={{ color: "#000000" }} />
+        </ListItemIcon>
+        <ListItemText primary="Setores" primaryTypographyProps={{ color: "#000000" }} />
+      </ListItem>
+    </Link>
+    <Link href="/listar/usuarios">
+      <ListItem button component="a">
+        <ListItemIcon >
+          <PersonIcon style={{ color: "#000000" }} />
+        </ListItemIcon>
+        <ListItemText primary="Usuários" primaryTypographyProps={{ color: "#000000" }} />
+      </ListItem>
+    </Link>
   </>
 );
