bb4f60f1849edf0059d1e8a02fd61e916b494ad9
rafael-14 <fcramos12@hotmail.com>
2022-07-07 13:37:23 -0300

    adjust theme in all pages

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,14 +1,23 @@
-import { useRouter } from "next/router";
 import React, { useEffect, useState } from 'react';
 import api from '../services/api';
 import { login, logout, getToken } from '../services/auth';
 import Login from "./login"
-import { Button, styled, CssBaseline, Box, Toolbar, List, Divider, IconButton, Grid } from '@mui/material';
+import {
+  Button, styled, CssBaseline, Box, createTheme, ThemeProvider,
+  Toolbar, List, Divider, IconButton, Grid
+} from '@mui/material';
 import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar from '@mui/material/AppBar';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { mainListItems } from '../components/listItems';
+
+const theme = createTheme({
+  palette: {
+    primary: { main: '#E8927C' },
+    secondary: { main: '#000000' }
+  }
+})
 
 const drawerWidth = 240;
 
@@ -85,56 +94,58 @@ export default function MyApp({ Component, pageProps }) {
   }
 
   return (
-    !redirect ?
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" style={{ background: '#E8927C' }} open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
+    <ThemeProvider theme={theme}>
+      {!redirect ?
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppBar position="absolute" style={{ background: '#E8927C' }} open={open}>
+            <Toolbar
               sx={{
-                color: "#FFFFFF",
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
+                pr: '24px', // keep right padding when drawer closed
               }}
             >
-              <MenuIcon />
-            </IconButton>
-            <Grid
-              container
-              direction="row"
-              justifyContent="space-between"
+              <IconButton
+                edge="start"
+                aria-label="open drawer"
+                onClick={toggleDrawer}
+                sx={{
+                  color: "#FFFFFF",
+                  marginRight: '36px',
+                  ...(open && { display: 'none' }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+              >
+                <Button style={{ color: '#FFFFFF', hover: '#FFFFFF' }} href="/"><strong>Virtual Joias ERP</strong></Button>
+                <Button style={{ color: '#FFFFFF', hover: '#FFFFFF' }} onClick={() => handleLogout()}><strong>Sair</strong></Button>
+              </Grid>
+            </Toolbar>
+          </AppBar>
+          <Drawer variant="permanent" open={open}>
+            <Toolbar
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-end',
+                px: [1],
+              }}
             >
-              <Button style={{ color: '#FFFFFF', hover: '#FFFFFF' }} href="/"><strong>Virtual Joias ERP</strong></Button>
-              <Button style={{ color: '#FFFFFF', hover: '#FFFFFF' }} onClick={() => handleLogout()}><strong>Sair</strong></Button>
-            </Grid>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List>{mainListItems}</List>
-          <Divider />
-        </Drawer>
-        <Component {...pageProps} />
-      </Box>
-      : <Login />
+              <IconButton onClick={toggleDrawer}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List>{mainListItems}</List>
+            <Divider />
+          </Drawer>
+          <Component {...pageProps} />
+        </Box>
+        : <Login />}
+    </ThemeProvider>
   )
 }
