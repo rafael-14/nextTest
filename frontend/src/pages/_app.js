4bf4b853b83e4d30744d60197c606aeb15fdb0e3
rafael-14 <fcramos12@hotmail.com>
2022-07-07 01:01:49 -0300

    redirect token workin

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,14 +1,30 @@
 import { useRouter } from "next/router";
+import Dashboard from "../components/Dashboard"
+//<Dashboard componente={router.pathname}/>
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+import { login, logout, getToken } from '../services/auth';
+import Login from "./login"
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
 
-  const router = useRouter();
+  const router = useRouter()
+
+  const [redirect, setRedirect] = useState(false)
+
+  useEffect(() => {
+    async function verifyToken() {
+      var res = await api.get('/api/check/token', { params: { token: getToken() } })
+      if (res.data.status !== 200) {
+        logout()
+        setRedirect(true);
+      }
+    }
+    verifyToken();
+  }, [])
 
   return (
-    <>
-      <Component {...pageProps} />
-    </>
+    !redirect ? <Component {...pageProps} /> : //router.push("/login")
+    <Login/>
   )
 }
-
-export default MyApp
